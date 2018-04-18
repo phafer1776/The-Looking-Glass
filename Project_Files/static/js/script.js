8e32161b84bb1e05ba28c92ec52f4765a54780ad
Unknown <phafer@mail.usf.edu>
2018-04-18 09:16:25 -0400

    Added javascript for login
    Still needs a little work.

--- Project_Files/static/js/script.js
+++ Project_Files/static/js/script.js
@@ -1,8 +1,41 @@
-function show_overlay() {
-    document.getElementById("overlay").style.display = "block";
-    document.getElementById("overlay-inner").style.display = "block"
-}
+$(document).ready(function() {
+    $('#signup-link').on('click', function () {
+        document.getElementById("overlay").style.display = "block";
+        document.getElementById("overlay-inner").style.display = "block"
+    });
+    $('#login-button').on('click', function() {
+        $.ajax({
+            url: '/Login',
+            data: $('#form-login').serialize(),
+            type: 'POST',
+            success: function(response) {
+                console.log(response);
+                if (response.authentication === true) {
+                    localStorage.setItem('userdata', JSON.stringify(response.user));
+                    populateUser();
+                }
+                else {
+                    $('#errorMessageLogin').text('Incorrect login information.');
+                }
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    });
 
-function hide_overlay() {
-    document.getElementById("overlay").style.display = "none";
-}
+    // function show_overlay() {
+    //     document.getElementById("overlay").style.display = "block";
+    //     document.getElementById("overlay-inner").style.display = "block"
+    // }
+    //
+    // function hide_overlay() {
+    //     document.getElementById("overlay").style.display = "none";
+    // }
+
+    function populateUser() {
+        var user = JSON.parse(localStorage.getItem('userdata'));
+        console.log(user);
+        $('#greeting').append(user.firstName);
+    }
+});
